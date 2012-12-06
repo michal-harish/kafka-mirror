47b2f1f5116fdaa669a54adad368401ac76cc347
Michal Haris <michal.haris@visualdna.com>
2012-12-06 15:56:00 +0000

    EDA-36 Modified resolver for mirroring between multiple central clusters, e.g. from prod to stag.

--- src/main/java/net/imagini/kafka/DestinationResolver.java
+++ src/main/java/net/imagini/kafka/DestinationResolver.java
@@ -5,6 +5,7 @@ import java.nio.ByteBuffer;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.UUID;
 
 import kafka.message.Message;
@@ -39,7 +40,6 @@ public class DestinationResolver  implements MirrorResolver
 
     static private Logger log = LoggerFactory.getLogger(DestinationResolver.class);
     static private JsonFactory jsonFactory = new JsonFactory();
-    final private List<MirrorDestination> emptyDestinationList = new ArrayList<MirrorDestination>();
 
     /**
      * Entry point method which decides how to resolve the message depending
@@ -47,71 +47,24 @@ public class DestinationResolver  implements MirrorResolver
      */
     public List<MirrorDestination> resolve(MessageAndMetadata<Message> metaMsg)
     {
-        if (metaMsg.topic().equals("tracking_events"))
-        {
-            return resolveTrackingEvents(metaMsg);
-        } else {
-            log.warn("Unknown topic " + metaMsg.topic());
-            return emptyDestinationList;
-        }
-    }
-
-    private List<MirrorDestination> resolveTrackingEvents(MessageAndMetadata<Message> metaMsg)
-    {
         ArrayList<MirrorDestination> result = new ArrayList<MirrorDestination>();
         try {
-            //get payload bytes
-            ByteBuffer buffer = metaMsg.message().payload();
-            byte [] bytes = new byte[buffer.remaining()];
-            buffer.get(bytes);
-
             //prepare fields we're interetsed in for parititoning purpose
-            HashMap<String,String> fields = new HashMap<String,String>();
+            Map<String,String> fields = new HashMap<String,String>();
             fields.put("timestamp", null);
             fields.put("date", null);
+            fields.put("event_type", null);
+            fields.put("userUid", null);
+
             fields.put("action", null);
             fields.put("objType", null);
             fields.put("objId", null);
-            fields.put("userUid", null);
             fields.put("vdna_widget_mc", null);
 
-            //read only the necessary fields (streaming jackson)
             try {
-                JsonParser jp = jsonFactory.createJsonParser(bytes);
-                int filled = 0;
-                while(jp.nextToken() != null && filled < fields.size())
-                {
-                    if (jp.getCurrentToken() == JsonToken.FIELD_NAME) {
-                        String fieldName = jp.getCurrentName();
-                        jp.nextToken();
-                        String value = jp.getText();
-                        if (fieldName.equals("event_type") && !value.equals("esVDNAAppUserActionEvent"))
-                        {
-                            throw new IOException("Invalid event_type = \""+value+"\"");
-                        } else if (fields.containsKey(fieldName))
-                        {
-                            value = value.equals("0") ? null : value;
-                            //log.debug("EVENT FIELD " + fieldName + " " + value);
-                            fields.put(fieldName, value);
-                            ++filled;
-                        }
-                    }
-                }
-                jp.close();
-            } catch (JsonParseException e) {
-                log.warn("Message not in JSON format: " + new String(bytes)) ;
-                e.printStackTrace();
-                return result;
+                parseMinimumJsonMessage(metaMsg.message(), fields);
             } catch (IOException e) {
                 log.error("Couldn'r read JSON ", e);
-                return result;
-            }
-
-            //deterime and validate derived values
-            String action = fields.get("action");
-            if (action == null)
-            {
-                log.warn("Invalid event with null action" + new String(bytes)) ;
                 return result;
             }
 
@@ -139,81 +92,99 @@ public class DestinationResolver  implements MirrorResolver
                 }
             }
 
-            /*
-             * Publish into one of the topics for each event type - e.g. primary topic.
-             * Every event has to end up at least in one of the following.
-             */
-            if (action.startsWith("INSERTED_INTO_"))
+            //now check event type and resolve accordingly
+            String eventType = fields.get("event_type");
+            if (eventType.equals("esVDNAAppUserActionEvent")) // event tracker message
             {
-                result.add(new MirrorDestination(
-                    "datasync", 
-                    uuidHash
-                ));
-            }
-            else if (action.equals("CONVERSION") && fields.get("objId").equals("sync"))
-            {
-                result.add(new MirrorDestination(
-                    "datasync", 
-                    uuidHash
-                ));
-            }
-            else if (action.equals("MINTED_USER_KEY"))
-            {
-                result.add(new MirrorDestination(
-                    "datasync", 
-                    uuidHash
-                ));
-            }
-            else if (action.equals("CONVERSION") && fields.get("objType").equals("CONVERSION"))
-            {
-                String conversionId = fields.get("objId");
-                if (conversionId.equals("loaded_quiz")
-                    || conversionId.equals("started_quiz")
-                    || conversionId.equals("loaded_results")
-                ) {
+                String action = fields.get("action");
+                if (action == null)
+                {
+                    log.warn("Invalid esVDNAAppUserActionEvent with null action: " + metaMsg.message().payload().toString());
+                    return result;
+                }
+                /*
+                 * Publish into one of the topics for each event type - e.g. primary topic.
+                 * Every event has to end up at least in one of the following.
+                 */
+                if (action.startsWith("INSERTED_INTO_"))
+                {
                     result.add(new MirrorDestination(
-                        "useractivity",
-                        uuidHash
-                    ));
-                } else {
-                    result.add(new MirrorDestination(
-                        "conversions", 
+                        "datasync", 
                         uuidHash
                     ));
                 }
-            }
-            else if (action.equals("IMPRESSION") && fields.get("objType").equals("AD"))
-            {
+                else if (action.equals("CONVERSION") && fields.get("objId").equals("sync"))
+                {
+                    result.add(new MirrorDestination(
+                        "datasync", 
+                        uuidHash
+                    ));
+                }
+                else if (action.equals("MINTED_USER_KEY"))
+                {
+                    result.add(new MirrorDestination(
+                        "datasync", 
+                        uuidHash
+                    ));
+                }
+                else if (action.equals("CONVERSION") && fields.get("objType").equals("CONVERSION"))
+                {
+                    String conversionId = fields.get("objId");
+                    if (conversionId.equals("loaded_quiz")
+                        || conversionId.equals("started_quiz")
+                        || conversionId.equals("loaded_results")
+                    ) {
+                        result.add(new MirrorDestination(
+                            "useractivity",
+                            uuidHash
+                        ));
+                    } else {
+                        result.add(new MirrorDestination(
+                            "conversions", 
+                            uuidHash
+                        ));
+                    }
+                }
+                else if (action.equals("IMPRESSION") && fields.get("objType").equals("AD"))
+                {
+                    result.add(new MirrorDestination(
+                        "adviews", 
+                        uuidHash
+                    ));
+                }
+                else if (action.equals("CLICK") && fields.get("objType").equals("AD"))
+                {
+                    result.add(new MirrorDestination(
+                        "adclicks", 
+                        uuidHash
+                    ));
+                }
+                else if (action.equals("PAGE_VIEW"))
+                {
+                    result.add(new MirrorDestination(
+                        "pageviews", 
+                        uuidHash
+                    ));
+                }
+            } else if (eventType.equals("VDNAQuizUserAction")) { // quiz engine message
                 result.add(new MirrorDestination(
-                    "adviews", 
+                    "useractivity", 
                     uuidHash
                 ));
+            } else {
+                log.warn("Unknown event_type "  + eventType);
+                return result;
             }
-            else if (action.equals("CLICK") && fields.get("objType").equals("AD"))
-            {
-                result.add(new MirrorDestination(
-                    "adclicks", 
-                    uuidHash
-                ));
-            }
-            else if (action.equals("PAGE_VIEW"))
-            {
-                result.add(new MirrorDestination(
-                    "pageviews", 
-                    uuidHash
-                ));
-            }
-    
+
             /*
              * If the event didn't end up in any primary topic, it's a problem.
              */
             if (result.size() == 0)
             {
-                //&& action.equals("UPDATED_IDENTITY")// old cached event snippet - ignore
-                log.warn("No primary topic for event type `" + action + "`: " + new String(bytes));
+                log.warn("No primary topic for event type `" + eventType + "`: " + metaMsg.message().payload().toString());
             }
-    
-            //Collect metrics
+
+            //TODO Collect metrics JIRA-35 Implement MBean
             /*
             try {
                 Double timestamp = Double.valueOf(fields.get("timestamp")) * 1000;
@@ -235,5 +206,40 @@ public class DestinationResolver  implements MirrorResolver
         }
 
         return result;
+    }
+
+    private void parseMinimumJsonMessage(Message message, Map<String,String> fields) throws IOException
+    {
+        ByteBuffer buffer = message.payload();
+        byte [] bytes = new byte[buffer.remaining()];
+        buffer.get(bytes);
+        //read only the necessary fields (streaming jackson)
+        JsonParser jp;
+        try {
+            jp = jsonFactory.createJsonParser(bytes);
+        } catch (JsonParseException e) {
+            log.warn("Message not in JSON format: " + new String(bytes)) ;
+            throw new IOException("Invalid message json, see logs for more details", e);
+        }
+        try {
+            int filled = 0;
+            while(jp.nextToken() != null && filled < fields.size())
+            {
+                if (jp.getCurrentToken() == JsonToken.FIELD_NAME) {
+                    String fieldName = jp.getCurrentName();
+                    jp.nextToken();
+                    String value = jp.getText();
+                    if (fields.containsKey(fieldName))
+                    {
+                        value = value.equals("0") ? null : value;
+                        fields.put(fieldName, value);
+                        ++filled;
+                    }
+                }
+            }
+        } finally {
+            jp.close();
+        }
+
     }
 }
